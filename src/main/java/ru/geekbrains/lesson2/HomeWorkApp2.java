084c58b62517af0748f6047c2c88b5cfa02dcc57
LostKiro <jiaito@yandex.ru>
2021-11-10 23:53:18 +0300

    change2

--- src/main/java/ru/geekbrains/lesson2/HomeWorkApp2.java
+++ src/main/java/ru/geekbrains/lesson2/HomeWorkApp2.java
@@ -2,59 +2,42 @@ package ru.geekbrains.lesson2;
 
 import com.sun.source.util.SourcePositions;
 
+import javax.lang.model.element.Name;
+
 public class HomeWorkApp2 {
     public static void main(String[] args) {
-        within10and20();
-        isPositiveOrNegative();
-        isNegative();
-        printWordNTimes();
-        
+        System.out.println(within10and20(15, 5));
+        isPositiveOrNegative(-10);
+        System.out.println(isNegative(10));
+        printWordNTimes("Pavel", 5);
+        System.out.println();
+        System.out.println(yearBingo(2021));
     }
-
-    public static boolean within10and20() {
-        int x1 = 9;
-        int x2 = 1;
+    public static boolean within10and20(int x1, int x2) {
         int sum = x1 + x2;
-        if (sum >= 10 && sum <= 20) {
-            System.out.println("Число " + sum + " в пределах");
-            return true;
-        } else {
-            System.out.println("Число " + sum + " не в пределах");
-            return false;
-        }
+        if (sum >= 10 && sum <= 20)
+            return true; return false;
     }
-
-    public static void isPositiveOrNegative() {
-        int x = 0;
+    public static void isPositiveOrNegative(int x) {
         if (x >= 0) {
             System.out.println(x + " положительное число");
-        } else {
-            System.out.println(x + " число отрицательное");
-        }
+        } else System.out.println(x + " число отрицательное");
     }
-
-    public static boolean isNegative() {
-        int x = 1;
-        if (x >= 0) {
-            System.out.println(false);
-            return true;
-        } else {
-            System.out.println(true);
-            return false;
-        }
+    public static boolean isNegative(int x) {
+        if (x >= 0)
+            return true; return false;
     }
-
-    public static void printWordNTimes() {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Name");
-        }
+    public static void printWordNTimes(String Name, int number) {
+        if (number != 0) {
+        for (int i = 0; i < number; i++) {
+            System.out.print(Name + " ");}
+        } else
+        System.out.println("You can't put 0 ");
     }
-
-    public static boolean years(int year) {
+    public static boolean yearBingo(int year) {
         if (((year %4 == 0) && (year %100 != 0)) ||( year %400 ==0)){
             return true;
         }
         return false;
     }
 }
-
